48c383ee901fe6dfc1564d9dd69994b59137db92
Donald Coleman <donaldcoleman@Donalds-MacBook-Pro-2.local>
2019-12-29 13:39:00 -0800

    Adding to knexfile

--- knexfile.js
+++ knexfile.js
@@ -10,5 +10,21 @@ module.exports = {
     seeds: { directory: './data/seeds' },
   },
 
-  
+  testing: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
 };
